61138dacb65be401ad705c6a9a77b13af7fb6f1b
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-09-14 12:40:30 +0530

    made the design more pixel perfect

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename={basename}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </StrictMode>,
 )
